c2cf2d798358c56783a417826b9c7d915c0909d6
leekyunghee <qkqhlkh@gmail.com>
2016-07-25 20:44:27 +0900

    Initial commit

---
+++ src/main/java/leekyunghee/examples/tdd/transcodingService/App.java
@@ -0,0 +1,13 @@
+package leekyunghee.examples.tdd.transcodingService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
